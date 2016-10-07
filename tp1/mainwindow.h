566779d2f60eaa34d2a5ac65c77e1f28375dff95
Camthug <camille.clavaud@outlook.fr>
2016-10-07 15:44:10 +0200

    implémentation split

--- tp1/mainwindow.h
+++ tp1/mainwindow.h
@@ -12,8 +12,7 @@
 #include <QLabel>
 #include <QImageReader>
 #include <QPixmap>
-#include <QWindow>
-
+#include <QResizeEvent>
 
 class MainWindow : public QMainWindow
 {
@@ -26,24 +25,29 @@ public:
 private:
     void createMenus();
     void createActions();
+    void mousePressEvent(QMouseEvent *e);
+    void mouseReleaseEvent(QMouseEvent *e);
+    void resizeEvent ( QResizeEvent * event );
+    void openNewWindow(QImage img);
 
 private slots:
     void about();
     void open();
-    void openNewWindow();
-    void openNewWindow(QImage img);
     void split();
+    void crop();
 
 private:
-    QAction *actionAbout;
-    QAction *actionOpen;
-    QAction *actionQuit;
-    QAction *actionCrop;
-    QAction *actionSplit;
-    QLabel *imagedisplay;
+    QAction* actionAbout;
+    QAction* actionOpen;
+    QAction* actionQuit;
+    QAction* actionSplit;
+    QAction* actionCrop;
     QImage imageObject;
-    QWindow *mMyNewWindow;
-    QPixmap *pixelmap;
+  //  QWindow* mMyNewWindow;
+    QLabel* imagedisplay;
+    QPixmap pixelmap;
+    QPoint point1;
+    QPoint point2;
 };
 
 #endif // MAINWINDOW_H
