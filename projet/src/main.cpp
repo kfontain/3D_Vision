ea273fe10320174ba76be6850a56265048bc0430
kfontain <fontaken@gmail.com>
2017-02-17 15:14:44 +0100

    updated main for scripting

--- projet/src/main.cpp
+++ projet/src/main.cpp
@@ -1,11 +1,33 @@
 #include "headers/mainwindow.h"
+#include "headers/include.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
 
-    return a.exec();
+
+    if (argc == 1)
+    {
+        QApplication a(argc, argv);
+        MainWindow w;
+        w.show();
+
+        return a.exec();
+    }
+
+    if (argc == 3)
+    {
+    //param
+    cv::Mat left = cv::imread(argv[1]);
+    cv::Mat right = cv::imread(argv[2]);
+
+    cv::Mat result;
+
+    dispMap(left, right, &result);
+
+    cv::imwrite("filename.jpg", result); //incrémenter le nom du fichier
+    //automatiser les SS sur Unity.
+    return 0;
+    }
+
 }
