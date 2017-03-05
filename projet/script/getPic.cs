456b2927d6d76c139121bb322a5858afdc7165a7
kfontain <fontaken@gmail.com>
2017-03-05 21:50:05 +0100

    automated screenshot + camera/bob movement

--- projet/script/getPic.cs
+++ projet/script/getPic.cs
@@ -1,32 +1,60 @@
-﻿// Saves screenshot as PNG file.
+// Saves screenshot as PNG file.
 using UnityEngine;
 using System.Collections;
 using System.IO;
 
 public class getPic : MonoBehaviour {
-	// Take a shot immediately
-	IEnumerator Start () {
-		yield return UploadPNG();
+
+	void Start () {
+
 	}
 
-	IEnumerator UploadPNG() {
-		// We should only read the screen buffer after rendering is complete
-		yield return new WaitForEndOfFrame ();
+	void move (string tag, char c, float dist) {
+		Vector3 position = GameObject.FindGameObjectWithTag (tag).transform.position;
+		if (c.Equals('x')) position.x = position.x + dist;
+		if (c.Equals('y')) position.y = position.y + dist;
+		if (c.Equals('z')) position.z = position.z + dist;
+		GameObject.FindGameObjectWithTag (tag).transform.position = position;
+	}
 
-		// Create a texture the size of the screen, RGB24 format
-		int width = Screen.width;
-		int height = Screen.height;
-		Texture2D tex = new Texture2D (width, height, TextureFormat.RGB24, false);
+	void reset (string tag, Vector3 initial) {
+		GameObject.FindGameObjectWithTag (tag).transform.position = initial;
+	}
 
-		// Read screen contents into the texture
-		tex.ReadPixels (new Rect (0, 0, width, height), 0, 0);
-		tex.Apply ();
+	private IEnumerator SS() {
+		Vector3 tmp = GameObject.FindGameObjectWithTag ("bob").transform.position;
+		for (int j = 0; j < 3; j++) {
+			for (int i = 0; i < 5; i++) {
+				// Wait untill we actually rendered
+				yield return new WaitForEndOfFrame ();
 
-		// Encode texture into PNG
-		byte[] bytes = tex.EncodeToPNG ();
-		Object.Destroy (tex);
+				Texture2D texture = new Texture2D (Screen.width, Screen.height, TextureFormat.RGB24, false);
 
-		// 
-		File.WriteAllBytes ("/Users/kenjif/Documents/cours/projetCV/3D_Vision/unity/pics/test.png", bytes);
+				// Read screen contents into the texture
+				texture.ReadPixels (new Rect (0, 0, Screen.width, Screen.height), 0, 0);
+				texture.Apply ();
+
+				// Write to file
+				byte[] bytes = texture.EncodeToPNG ();
+				System.IO.File.WriteAllBytes ("/Users/kenjif/Documents/cours/projetCV/3D_Vision/unity/pics/cam" + j + "-dist" + i + ".png", bytes);
+
+				// Clean up the used texture
+				Destroy (texture);
+
+				// move bob
+				move ("bob", 'z', 1);
+			}
+			// reset bob to initial position and move each camera
+			reset ("bob", tmp);
+			move ("camera_left" , 'x', -0.05f);
+			move ("camera_right", 'x',  0.05f);
+		}
+	}
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.K)) {
+			StartCoroutine (SS ());
+		}
 	}
 }
