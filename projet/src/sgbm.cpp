c0d8b3816924ba509eb76fba798fee6f1a6da7f8
kfontain <fontaken@gmail.com>
2017-04-07 15:39:47 +0200

    added 4 trackbars

--- projet/src/sgbm.cpp
+++ projet/src/sgbm.cpp
@@ -6,6 +6,8 @@ cv::Mat g1, g2;
 cv::Mat disp, disp8;
 cv::StereoBM sbm;
 int sad, nbdisp;
+int mindisp;
+int uniq;
 
 /// Cette fonction calcule la carte de disparité à partir des images sources en paramètres.
 /// Cette fonction utilise l'algorithme SGBM.
@@ -19,52 +21,47 @@ void dispSGBM()
 
     Mat img1, img2;
 
-    sad = 0;
-    nbdisp = 112;
+    sad = 8;
+    nbdisp = 0;
+    mindisp = 37;
+    uniq = 14;
 
-    img1 = imread("left_1.png");
+    img1 = imread("left_1.png"); //7cm
     img2 = imread("right_1.png");
 
     cvtColor(img1, g1, CV_BGR2GRAY);
     cvtColor(img2, g2, CV_BGR2GRAY);
 
-    sbm.state->SADWindowSize = 2*sad+5; //Doit être impair est compris entre 5 et 255.
-    sbm.state->numberOfDisparities = 112;
-    sbm.state->preFilterSize = 5;
-    sbm.state->preFilterCap = 61;
-    sbm.state->minDisparity = -39;
-    sbm.state->textureThreshold = 507;
-    sbm.state->uniquenessRatio = 0;
-    sbm.state->speckleWindowSize = 0;
-    sbm.state->speckleRange = 8;
-    sbm.state->disp12MaxDiff = 1;
-
-    sbm(g1, g2, disp);
-    normalize(disp, disp8, 0, 255, CV_MINMAX, CV_8U);
     namedWindow("SBM", WINDOW_NORMAL);
-    imshow("SBM", disp8);
 
-    std::string test = std::to_string(2*sad+1);
-    //N'arrive pas à afficher la valeur.
+    update(0, NULL);
 
-    createTrackbar("SAD : " + test, "SBM", &sad, 125, update);
+    createTrackbar("SAD", "SBM", &sad, 125, update);
+    createTrackbar("nb disp", "SBM", &nbdisp, 10, update);
+    createTrackbar("min disp", "SBM", &mindisp, 70, update);
+    createTrackbar("unique ratio", "SBM", &uniq, 20, update);
     //Reste les autres trackbar à finir en prenant en compte les valeurs min & max.
 }
 
 static void update(int, void*)
 {
-    //todo : delete previous sbm in "SBM" window.
-    fprintf(stderr, "bonjour \n");
-    sbm.state->SADWindowSize = 2*sad+1;
-    sbm.state->numberOfDisparities = 112;
-    sbm.state->preFilterSize = 5;
-    sbm.state->preFilterCap = 61;
-    sbm.state->minDisparity = -39;
-    sbm.state->textureThreshold = 507;
-    sbm.state->uniquenessRatio = 0;
+    sbm.state->SADWindowSize = 2*sad+5;
+    sbm.state->numberOfDisparities = 16*nbdisp + 16;
+    sbm.state->minDisparity = mindisp - 20;
+    sbm.state->uniquenessRatio = uniq;
+    fprintf(stderr, "SADWindowSize : %d ", sbm.state->SADWindowSize);
+    fprintf(stderr, "numberOfDisparities : %d ", sbm.state->numberOfDisparities);
+    fprintf(stderr, "minDisparity : %d ", sbm.state->minDisparity);
+    fprintf(stderr, "uniquenessRatio : %d ", sbm.state->uniquenessRatio);
+    fprintf(stderr, "\n----------\n");
+
+    sbm.state->preFilterSize = 51;
+    sbm.state->preFilterCap = 63;
+    sbm.state->textureThreshold = 30;
     sbm.state->speckleWindowSize = 0;
-    sbm.state->speckleRange = 8;
+    sbm.state->speckleRange = 4;
     sbm.state->disp12MaxDiff = 1;
+
     sbm(g1, g2, disp);
     normalize(disp, disp8, 0, 255, CV_MINMAX, CV_8U);
     imshow("SBM", disp8);
